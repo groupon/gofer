564886e6efb253da41dd22d49c097ad502a9b21f
Jan Krems <jan.krems@groupon.com>
2016-04-06 09:17:00 -0700

    fix: Working sub-classing

--- test/gofer.test.js
+++ test/gofer.test.js
@@ -1,10 +1,46 @@
 'use strict';
+var util = require('util');
+
 var assert = require('assertive');
 
 var Gofer = require('../');
 
+var withMockService = require('./mock-service');
+
 describe('gofer', function () {
+  var options = withMockService();
+
   it('exports a `fetch` function', function () {
     assert.hasType(Function, Gofer.fetch);
+  });
+
+  describe('direct usage', function () {
+    var gofer;
+    before('create Gofer instance', function () {
+      gofer = new Gofer().with(options);
+    });
+
+    it('can fetch something', function () {
+      return gofer.get('/echo');
+    });
+  });
+
+  describe('sub-class', function () {
+    function SubGofer(config) {
+      Gofer.call(this, config, 'sub', '1.2.3', 'my-sub-client');
+    }
+    util.inherits(SubGofer, Gofer);
+
+    var sub;
+    before('create SubGofer instance', function () {
+      sub = new SubGofer({ sub: options });
+    });
+
+    it('can fetch something', function () {
+      return sub.get('/echo').json()
+        .then(function (echo) {
+          assert.include('my-sub-client/1.2.3', echo.headers['user-agent']);
+        });
+    });
   });
 });
