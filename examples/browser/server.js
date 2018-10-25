7296fe77cc5be83d87f3bbe7d6c398a0c900e6a7
David Bushong <dbushong@groupon.com>
2018-10-24 19:39:43 -0700

    docs: update examples to use modern JS

--- examples/browser/server.js
+++ examples/browser/server.js
@@ -1,23 +1,26 @@
 'use strict';
-var fs = require('fs');
-var http = require('http');
 
-var browserify = require('browserify');
+const fs = require('fs');
+const http = require('http');
 
-var BASE_HTML = __dirname + '/base.html';
+const browserify = require('browserify');
+
+const BASE_HTML = `${__dirname}/base.html`;
 
 function onRequest(req, res) {
   if (req.url === '/favicon.ico') {
-    return res.end('ok');
+    return void res.end('ok');
   }
 
   if (req.url.indexOf('/echo') === 0) {
     res.setHeader('Content-Type', 'application/json; charset=utf8');
-    return res.end(JSON.stringify({
-      method: req.method,
-      headers: req.headers,
-      url: req.url,
-    }));
+    return void res.end(
+      JSON.stringify({
+        method: req.method,
+        headers: req.headers,
+        url: req.url,
+      })
+    );
   }
 
   function fail(error) {
@@ -26,10 +29,10 @@ function onRequest(req, res) {
 
   function onBundle(error, bundle) {
     if (error) {
-      return fail(error);
+      return void fail(error);
     }
-    var htmlTpl = fs.readFileSync(BASE_HTML, 'utf8');
-    var html = htmlTpl + '<script>' + bundle + '</script>';
+    const htmlTpl = fs.readFileSync(BASE_HTML, 'utf8');
+    const html = `${htmlTpl}<script>${bundle}</script>`;
     res.setHeader('Content-Type', 'text/html');
     res.end(html);
   }
@@ -43,6 +46,4 @@ function onListen() {
   process.stdout.write('Listening on http://127.0.0.1:3000\n');
 }
 
-http
-  .createServer(onRequest)
-  .listen(3000, onListen);
+http.createServer(onRequest).listen(3000, onListen);
